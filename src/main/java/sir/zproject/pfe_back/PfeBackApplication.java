68301f1a0f1b98900a2bc5b573e969cc2a715cdc
noujoud <ayaninoujoud1@gmail.com>
2024-04-23 16:11:22 +0100

    initial

---
+++ src/main/java/sir/zproject/pfe_back/PfeBackApplication.java
@@ -0,0 +1,13 @@
+package sir.zproject.pfe_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sir/zproject/pfe_back/PfeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package sir.zproject.pfe_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
