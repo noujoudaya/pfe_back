db69ad07c28a89838936cf83a648c9e41070e5fb
AablaEssadaq <aabla2003@gmail.com>
2024-05-04 13:39:50 +0100

    fixed Libellé in typeConge and import problem in ServiceDao

--- src/main/java/sir/zproject/pfe_back/dao/ServiceDao.java
+++ src/main/java/sir/zproject/pfe_back/dao/ServiceDao.java
@@ -2,7 +2,8 @@ package sir.zproject.pfe_back.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
+import sir.zproject.pfe_back.bean.Service;
+
 
 @Repository
 public interface ServiceDao extends JpaRepository<Service, Long> {
